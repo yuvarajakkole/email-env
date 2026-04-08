703dc0aeab0be8b062a8c4e9dd87230ed42dcfbf
Yuvaraj Akkole <yuvarajakkole@gmail.com>
2026-04-08 13:58:54 +0530

    fix main callable for openenv

--- server/app.py
+++ server/app.py
@@ -5,3 +5,7 @@ import os
 def main():
     port = int(os.environ.get("PORT", 7860))
     uvicorn.run(app, host="0.0.0.0", port=port)
+
+# 🔥 THIS LINE IS MISSING (VERY IMPORTANT)
+if __name__ == "__main__":
+    main()
